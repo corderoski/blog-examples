5f7f99288a9821bc7fea505be2218c51e3cf3b79
José Cordero <jcorderodr@users.noreply.github.com>
2015-05-26 11:08:52 -0400

    - add databases/oracle/ > ConsoleApplication1 testing a basic Oracle's connection.

---
+++ databases/oracle/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+using System.Configuration;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbProviderFactory factory = DbProviderFactories.GetFactory("Oracle.DataAccess.Client");
+            var conn = factory.CreateConnection();
+
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+
+            bool wasOpened = false;
+            try
+            {
+                conn.Open();
+                wasOpened = conn.State == System.Data.ConnectionState.Open;
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            Console.WriteLine("The Connection {0} opened.", wasOpened ? "was" : "wasn´t");
+            Console.Read();
+        }
+    }
+}
