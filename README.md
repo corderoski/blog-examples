14783f1dc4a79b0a305191cda47004c056e4d0d1
jcordero-ede <jcordero-ede@localhost>
2015-05-26 14:12:46 -0400

    - add basic Readme - add microsoft-technologies/activeDirectory > Helpers - add System Center Service Manager class (Details: https://bitbucket.org/corderoski/scsm-lite-tool)

---
+++ README.md
@@ -0,0 +1,14 @@
+# README #
+
+### What is this repository for? ###
+
+* A repository of samples, few simple codes and random stuff.
+
+### How do I get set up? ###
+
+* Download
+* Compile
+* Configuration in some cases where apply (like db providers).
+
+No extra files. Life should be this easier ¿Right?
+
---
+++ microsoft-tech/activeDirectory/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestAD.Entities
+{
+	
+    public class User
+    {
+        public Int64 EmployeeId { get; set; }
+        public String UserName { get; set; }
+        public String DisplayName { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public String Description { get; set; }
+        public String Department { get; set; }
+        public String EMail { get; set; }
+        public String Phone { get; set; }
+        public Manager Supervisor { get; set; }
+    }
+
+    public struct Manager
+    {
+        public int EmployeeId { get; set; }
+        public String Name { get; set; }
+    }
+    
+}
